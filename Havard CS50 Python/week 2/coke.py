1a4c27031d66f7dced629c48bfc53a492194cc80
劉宣亭 Fiona Liou <shuanliou@gmail.com>
2025-10-28 00:32:39 +0800

    Create coke.py

---
+++ Havard CS50 Python/week 2/coke.py
@@ -0,0 +1,13 @@
+def main():
+    amount = 50
+    while amount > 0:
+        print(f"Amount Due: {amount}")
+        coin = int(input("Insert Coin: "))
+        if coin in (25, 10, 5):
+            amount -= coin
+
+    print(f"Change Owed: {-amount}")
+
+
+if __name__ == "__main__":
+    main()
